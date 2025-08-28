5cce88b920f9a6750866757492086f5a980627fd
jjhur <soss3264@gmail.com>
2025-08-28 20:18:36 +0900

    Add chapter 4 code

---
+++ scratch_agents/tools/__init__.py
@@ -0,0 +1,5 @@
+from .search_web import search_web
+from .wikipedia import search_wikipedia, get_wikipedia_page
+from .calculator import calculator
+
+__all__ = ["search_web", "search_wikipedia", "get_wikipedia_page", "calculator"]
